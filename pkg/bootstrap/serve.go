812dab4789ecf10efc174617031a70902986c601
Junseong kim <anima94@kakao.com>
2024-01-24 21:25:48 +0900

    feat: static package 추가
    with mario world :mushroom:

--- pkg/bootstrap/serve.go
+++ pkg/bootstrap/serve.go
@@ -4,12 +4,15 @@ import (
 	"Gin-blogproject/pkg/config"
 	"Gin-blogproject/pkg/html"
 	"Gin-blogproject/pkg/routing"
+	"Gin-blogproject/pkg/static"
 )
 
 func Serve() {
 	config.Set()
 
 	routing.Init()
+
+	static.LoadStatic(routing.GetRouter())
 
 	html.LoadHTML(routing.GetRouter())
 
