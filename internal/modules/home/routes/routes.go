41d1abba98a1bd500b1774355ee1b8d347201eba
Junseong kim <anima94@kakao.com>
2024-01-27 11:31:24 +0900

    feat: homepage 기능 추가

--- internal/modules/home/routes/routes.go
+++ internal/modules/home/routes/routes.go
@@ -1,21 +1,12 @@
 package routes
 
 import (
-	"Gin-blogproject/pkg/html"
+	homeCtrl "Gin-blogproject/internal/modules/home/controllers"
 	"github.com/gin-gonic/gin"
-	"net/http"
 )
 
 func Routes(router *gin.Engine) {
-	router.GET("/", func(c *gin.Context) {
-		html.Render(c, http.StatusOK, "modules/home/html/home", gin.H{
-			"title": "Home page",
-		})
-	})
 
-	router.GET("/about", func(c *gin.Context) {
-		html.Render(c, http.StatusOK, "modules/home/html/about", gin.H{
-			"title": "About page",
-		})
-	})
+	homeController := homeCtrl.New()
+	router.GET("/", homeController.Index)
 }
